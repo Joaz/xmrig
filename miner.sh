b07604ae94dde169203dafedc0a6713fcaa252aa
Jennifer Glauche <=^.^=@server2.jglauche.de>
2017-11-30 11:58:03 +0100

    nicely working binary and startup script

---
+++ miner.sh
@@ -0,0 +1,2 @@
+cd ~/xmrig
+./xmrig -o stratum+tcp://etn.easyhash.io:3630 -u etnkGJEqnyk2PpnnfFSHr1Ab8XrAPefKzAZjw2Kr4kZpedJiPhSUUhS1TXNw5yatn69HzWdDvPneWHLRYHMT5DwG16ZEPsrzps -k --cpu-affinity 0xF -t 4 --av 1
